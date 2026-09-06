b4c1c2ce6d0af673548e166fc1a4c5d86fadd594
prince sankhla <princesankhala670@gmail.com>
2026-09-06 23:43:31 +0530

    feat(field): expose unified field scanner API

--- sentry_field/vision/__init__.py
+++ sentry_field/vision/__init__.py
@@ -1,5 +1,6 @@
 """SENTRY FIELD vision runtime."""
 
 from .engine import run_pothole_demo
+from .scanner import FieldScanner, run_field_scanner
 
-__all__ = ["run_pothole_demo"]
+__all__ = ["FieldScanner", "run_field_scanner", "run_pothole_demo"]
