03d95e65e8e9b48d082f423d352799d3eab36c8a
prince sankhla <princesankhala670@gmail.com>
2026-09-06 23:47:00 +0530

    fix field scanner module entrypoint

--- sentry_field/scripts/run_field_scanner.py
+++ sentry_field/scripts/run_field_scanner.py
@@ -1,10 +1,5 @@
-import os
-
 from sentry_field.vision.scanner import run_field_scanner
 
 
 if __name__ == "__main__":
-    source = os.getenv("SENTRY_CAMERA_URL", "")
-    if source:
-        print(f"camera: {source}")
     run_field_scanner()
