0fe34602b8171b5c669029f68830f01fad9628d9
prince sankhla <princesankhala670@gmail.com>
2026-09-07 19:20:27 +0530

    feat(field): wire command center into field route

--- frontend/src/app/field/page.tsx
+++ frontend/src/app/field/page.tsx
@@ -1,12 +1,12 @@
 import { PageShell } from "@/components/ui/page";
-import { FieldWorkspaceFast } from "@/components/field/workspace_fast";
+import { FieldWorkspaceCommandCenter } from "@/components/field/workspace_command_center";
 
 export const dynamic = "force-dynamic";
 
 export default function FieldPage() {
   return (
     <PageShell>
-      <FieldWorkspaceFast />
+      <FieldWorkspaceCommandCenter />
     </PageShell>
   );
 }
