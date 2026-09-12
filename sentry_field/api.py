2feaeda4fb7f68d6ed9316c3d0018770d576171b
prince sankhla <princesankhala670@gmail.com>
2026-09-12 14:50:19 +0530

    FIELD: route canonical gateway through browser-compatible raw camera stream

--- sentry_field/api.py
+++ sentry_field/api.py
@@ -5,7 +5,7 @@ from urllib.parse import urlparse
 
 from . import api_v2 as _gateway
 from .api_robust import FIELD_API_PORT, app
-from .fast_stream_socket import stream as _fast_stream
+from .raw_camera_stream import stream as _fast_stream
 from starlette.requests import Request
 from starlette.responses import Response, StreamingResponse
 
